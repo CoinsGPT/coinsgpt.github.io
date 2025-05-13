2cd2b9c5763d950542ee66c955fd6a3a1d119100
CoinsGPT <gitcoins@outlook.com>
2025-05-13 11:50:35 +0800

    Update docusaurus.config.ts

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * LICENSE file in the root directory of this source tree .
  */
 import path from 'path';
 import npm2yarn from '@docusaurus/remark-plugin-npm2yarn';
